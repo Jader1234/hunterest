9d64d74d37f2bcd3056cdfdce0e29ce412bc6c90
jcrossignolli@yahoo.com.br <jcrossignolli@yahoo.com.br>
2020-07-30 20:34:59 -0300

    asas

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const path = require('path');
 const app = express();
 
 // Serve static files....
-app.use(express.static(__dirname + '/hunterest/'));
+app.use(express.static(__dirname + '/hunterest-front/'));
 
 // Send all requests to index.html
 app.get('/', function(req, res) {
