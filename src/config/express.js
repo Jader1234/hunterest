9c12b4e212e3927cf3f4d58b4c3d62086929ba66
jcrossignolli@yahoo.com.br <jcrossignolli@yahoo.com.br>
2020-07-30 21:46:32 -0300

    jnjnfajs

--- src/config/express.js
+++ src/config/express.js
@@ -6,7 +6,7 @@ module.exports = function(){
     var app = express();
     app.set('port', 3000);
     //Middleware
-    app.use(express.static('./public'));
+    app.use(express.static('./app.component.html'));
 	app.use(bodyParser.urlencoded({extended: true}));
 	app.use(bodyParser.json());
 	app.set(require('method-override')());
