f2668bdd65ab541629af8a2a14c8bed5b59fe10a
jcrossignolli@yahoo.com.br <jcrossignolli@yahoo.com.br>
2020-08-01 17:35:47 -0300

    kasnf

--- src/app/server.js
+++ src/app/server.js
@@ -1,6 +1,6 @@
 var http = require('http');
 var app = require('./config/express')();
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 3000
 http.createServer(app).listen(PORT), function(){
     console.log('Express Server escutando na porta ' + app.get('port'));
 };
