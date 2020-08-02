3df96a6bc0051acb54e48282dccf23d25c02f7f1
jcrossignolli@yahoo.com.br <jcrossignolli@yahoo.com.br>
2020-08-02 18:22:14 -0300

    ansf

--- server.js
+++ server.js
@@ -3,10 +3,10 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static(`${__dirname}/dist/hunterest`));
+app.use(express.static(__dirname + '/dist/hunterest'));
 
 app.get('/*', (req, res) => {
-res.sendFile(path.join(`${__dirname}/dist/hunterest/index.html`));
+res.sendFile(path.join(__dirname + '/dist/hunterest/index.html'));
 });
 
 app.listen(process.env.PORT || 8080);
